6f06ce34a820a8893c1db654a1fd3c6608f6adbd
xx51xx <live2@qq.com>
2025-08-02 09:02:52 +0800

    -

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,7 +9,7 @@
 # sed -i 's/pcdata(boardinfo.model/pcdata(boardinfo.system/g' package/lean/autocore/files/x86/index.htm
 #
 # 修改内核版本,版本修改为:5.4
-# sed -i 's/KERNEL_PATCHVER:=\([0-9]\+\.[0-9]\+\)/KERNEL_PATCHVER:=5.4/g' target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=\([0-9]\+\.[0-9]\+\)/KERNEL_PATCHVER:=5.4/g' target/linux/x86/Makefile
 #
 # 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
 # sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
