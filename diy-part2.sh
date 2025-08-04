f7a482e48cf1f5ea7c1d9ae9b5cedcc9d0cb100e
xx51xx <live2@qq.com>
2025-08-04 19:19:40 +0800

    -

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,64 +1,67 @@
 #!/bin/bash
+
 # 修改 BusyBox Shell 启动信息
 find . -type f -name 'messages.c' | while read -r file; do
-  sed -i 's|const char bb_banner\[\] ALIGN1 = "BusyBox v" BB_VER BB_EXTRA_VERSION;|const char bb_banner[] ALIGN1 = "Welcome to OpenWRT";|' "$file"
+  echo "已查到文件:$file"
+  sed -i 's|const char bb_banner\[\].*=[^;]*;|const char bb_banner[] ALIGN1 = "Welcome to OpenWRT";|' "$file"
 done
 
-# 修改 openwrt_release 显示信息
-find . -type f -name 'openwrt_release' | while read -r file; do
-    # 使用 sed 替换 DISTRIB_DESCRIPTION 的值
-    sed -i 's|^DISTRIB_DESCRIPTION=.*|DISTRIB_DESCRIPTION=`OpenWRT `|' "$file"
-done
+# 修改 openwrt_release 显示信息 package/base-files/files/etc/openwrt_release
+sed -i 's|^DISTRIB_DESCRIPTION=.*|DISTRIB_DESCRIPTION="OpenWRT"|' package/base-files/files/etc/openwrt_release
 
-# 修改 footer.ut 尾部信息
-find . -type f -name 'footer.ut' | while read -r file; do
 
-    awk '
-    BEGIN { in_block = 0 }
+# 修改 footer.ut 尾部信息 feeds/luci/themes/luci-theme-bootstrap/ucode/template/themes/bootstrap/footer.ut
+file="feeds/luci/themes/luci-theme-bootstrap/ucode/template/themes/bootstrap/footer.ut"
 
-    {
-        if ($0 ~ /{% if \(!blank_page\): %}/) {
-            in_block = 1
-            print "{% if (!blank_page): %}"
-            print "    </div>"
-            print "    <footer>"
-            print "        <span>"
-            print "            &copy; ${new Date().getFullYear()} 两笙山世 私人定制版OpenWRT  |  联系邮箱: live2@qq.com"
-            print "        </span>"
-            print "    </footer>"
-            print "    <script>"
-            print "    L.require(\"menu-bootstrap\");"
-            print ""
-            print "    (function () {"
-            print "        var loginBtn = document.querySelector(\"button.btn.cbi-button-positive.important\");"
-            print "        if (loginBtn) {"
-            print "            const p = document.createElement(\"p\");"
-            print "            p.style.cssText = \"text-align:center;padding:10px;color:#888;font-size:14px;\";"
-            print "            p.textContent = \"本站仅用于个人学习与作品集展示，不提供对外服务，感谢您的访问。\";"
-            print "            document.querySelector(\"footer\")?.appendChild(p);"
-            print "        }"
-            print "    })();"
-            print "    </script>"
-            print "{% endif %}"
-            next
-        }
+awk '
+BEGIN { in_block = 0 }
 
-        if (in_block && $0 ~ /<\/html>/) {
-            in_block = 0
-            print "</body>"
-            print "</html>"
-            next
-        }
+{
+    if ($0 ~ /{% if \(!blank_page\): %}/) {
+        in_block = 1
+        print "{% if (!blank_page): %}"
+        print "    </div>"
+        print "    <footer>"
+        print "        <span>"
+        print "            &copy; <span id=\"year\"></span> 两笙山世 私人定制版OpenWRT  |  联系邮箱: live2@qq.com"
+        print "        </span>"
+        print "    </footer>"
+        print "    <script>"
+        print "    L.require(\"menu-bootstrap\");"
+        print ""
+        print "    (function () {"
+        print "        var loginBtn = document.querySelector(\"button.btn.cbi-button-positive.important\");"
+        print "        if (loginBtn) {"
+        print "            const p = document.createElement(\"p\");"
+        print "            p.style.cssText = \"text-align:center;padding:10px;color:#888;font-size:14px;\";"
+        print "            p.innerHTML = \"&copy; <span id=\\\"year2\\\"></span> 两笙山世 私人定制版OpenWRT  |  联系邮箱: live2@qq.com\";"
+        print "            document.querySelector(\"footer\")?.appendChild(p);"
+        print "            document.getElementById(\"year2\").textContent = new Date().getFullYear();"
+        print "        }"
+        print "        document.getElementById(\"year\").textContent = new Date().getFullYear();"
+        print "    })();"
+        print "    </script>"
+        print "{% endif %}"
+        next
+    }
 
-        if (!in_block)
-            print $0
-    }' "$file" > "$file.tmp" && mv "$file.tmp" "$file"
-done
+    if (in_block && $0 ~ /<\/html>/) {
+        in_block = 0
+        print "</body>"
+        print "</html>"
+        next
+    }
 
-# 修改 sysauth.htm 显示版权
-find . -type f -name 'sysauth.htm' | while read -r file; do
-    # 使用 sed 替换 blank_page: true -> false
-    sed -i "s/{% include('footer', { *blank_page: *true *}) %}/{% include('footer', { blank_page: false }) %}/g" "$file"
-done
+    if (!in_block)
+        print $0
+}
+' "$file" > "$file.tmp" && mv "$file.tmp" "$file"
+
+
+
+# 修改 sysauth.htm 显示版权 feeds/luci/themes/luci-theme-bootstrap/ucode/template/themes/bootstrap/sysauth.htm
+sed -i -E "s/(blank_page:)[[:space:]]*true/\1 false/g" feeds/luci/themes/luci-theme-bootstrap/ucode/template/themes/bootstrap/sysauth.htm
+
+
 
 exit 0
