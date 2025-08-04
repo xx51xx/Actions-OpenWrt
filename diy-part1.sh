d59f0d67531bd39d402e35d65b3f0ca734a7f2dc
xx51xx <live2@qq.com>
2025-08-04 15:24:14 +0800

    -

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,5 +13,10 @@
 #
 # 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
 # sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
+# 修改 BusyBox Shell 启动信息
+find . -type f -name 'messages.c' | while read -r file; do
+  echo "Processing $file ..."
+  sed -i 's|const char bb_banner\[\] ALIGN1 = "BusyBox v" BB_VER BB_EXTRA_VERSION;|const char bb_banner[] ALIGN1 = "Welcome to OpenWRT";|' "$file"
+done
 
 exit 0
