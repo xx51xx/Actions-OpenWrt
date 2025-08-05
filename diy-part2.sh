4a2ebec68963a7669c11ed4a2c346ae5c06bbf94
xx51xx <live2@qq.com>
2025-08-05 15:38:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,12 +11,21 @@ endef\n' package/utils/busybox/Makefile
 # 修改 openwrt_release 显示信息 package/base-files/files/etc/openwrt_release
 sed -i "s|^DISTRIB_DESCRIPTION='%D %V %C'|DISTRIB_DESCRIPTION='OpenWRT'|" package/base-files/files/etc/openwrt_release
 
-# 修改 footer.ut 尾部信息 feeds/luci/themes/luci-theme-bootstrap/ucode/template/themes/bootstrap/footer.ut
-sed -i '/<footer>/,/<\/footer>/ s|\(<span>.*\)</span>|\1<br>&copy; <span id="year"></span> 两笙山世 私人定制版OpenWRT  \|  联系邮箱: live2@qq.com</span>|' feeds/luci/themes/luci-theme-bootstrap/ucode/template/themes/bootstrap/footer.ut
-sed -i 's|<script>L\.require('\''menu-bootstrap'\'')</script>|<script>\
-L.require('\''menu-bootstrap'\'');\
-document.getElementById("year").textContent = new Date().getFullYear();\
-</script>|' feeds/luci/themes/luci-theme-bootstrap/ucode/template/themes/bootstrap/footer.ut
+file="feeds/luci/themes/luci-theme-bootstrap/ucode/template/themes/bootstrap/footer.ut"
+sed -i '/{% if (!blank_page): %}/,/{% endif %}/c\
+{% if (!blank_page): %}\
+</div>\
+<footer>\
+  <span>\
+    &copy; <span id="year"></span> 两笙山世 私人定制版OpenWRT  |  联系邮箱: live2@qq.com\
+  </span>\
+  <ul class="breadcrumb pull-right" id="modemenu" style="display:none"></ul>\
+</footer>\
+<script>\
+  L.require('\''menu-bootstrap'\'');\
+  document.getElementById("year").textContent = new Date().getFullYear();\
+</script>\
+{% endif %}' "$file"
 
 
 # 修改 sysauth.htm 显示版权 feeds/luci/themes/luci-theme-bootstrap/ucode/template/themes/bootstrap/sysauth.ut
