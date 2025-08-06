adcab1cddd9713b17cc6cf7407f512b17f0cc691
xx51xx <live2@qq.com>
2025-08-06 11:57:21 +0800

    -

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,28 +8,16 @@ define Build/Prepare\n\
 \t$(SED) '\''s|const char bb_banner\\[\\].*=[^;]*;|const char bb_banner[] ALIGN1 = "Welcome to OpenWRT";|'\'' $(PKG_BUILD_DIR)/libbb/messages.c\n\
 endef\n' package/utils/busybox/Makefile
 
-# 修改 openwrt_release 显示信息 package/base-files/files/etc/openwrt_release
-sed -i "s|^DISTRIB_DESCRIPTION='%D %V %C'|DISTRIB_DESCRIPTION='OpenWRT'|" package/base-files/files/etc/openwrt_release
-
+# 修改 footer 信息 feeds/luci/themes/luci-theme-bootstrap/ucode/template/themes/bootstrap/footer.u
 file="feeds/luci/themes/luci-theme-bootstrap/ucode/template/themes/bootstrap/footer.ut"
 cat > "$file" <<'EOF'
 {% if (!blank_page): %}
 </div>
 <footer>
   <span>
-    Powered by
-    <a href="https://github.com/openwrt/luci" target="_blank" rel="noreferrer">
-      {{ version.luciname }} ({{ version.luciversion }})
-    </a>
-    /
-    <a href="{{ entityencode(version.disturl ?? '#', true) }}" target="_blank" rel="noreferrer">
-      {{ version.distname }} {{ version.distversion }} ({{ version.distrevision }})
-    </a>
-    {% if (lua_active): %}
-      / {{ _('Lua compatibility mode active') }}
-    {% endif %}
-    <br>
     &copy; <span id="year"></span> 两笙山世 私人定制版 OpenWRT | 联系邮箱: live2@qq.com
+    <br>
+    本站仅用于个人学习与作品集展示，不提供对外服务，感谢您的访问。
   </span>
   <ul class="breadcrumb pull-right" id="modemenu" style="display:none"></ul>
 </footer>
@@ -39,8 +27,6 @@ cat > "$file" <<'EOF'
 </script>
 {% endif %}
 EOF
-
-
 
 # 修改 sysauth.htm 显示版权 feeds/luci/themes/luci-theme-bootstrap/ucode/template/themes/bootstrap/sysauth.ut
 sed -i -E "s/(blank_page:)[[:space:]]*true/\1 false/g" feeds/luci/themes/luci-theme-bootstrap/ucode/template/themes/bootstrap/sysauth.ut
