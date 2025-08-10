936f9862d4d3c12aa5553e37c8972c016dd9ef6d
xx51xx <live2@qq.com>
2025-08-10 13:14:47 +0800

    -

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
 # 修改内核版本,版本修改为:5.4
-# sed -i 's/KERNEL_PATCHVER:=\([0-9]\+\.[0-9]\+\)/KERNEL_PATCHVER:=5.4/g' target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=\([0-9]\+\.[0-9]\+\)/KERNEL_PATCHVER:=5.4/g' target/linux/x86/Makefile
 #
 exit 0
