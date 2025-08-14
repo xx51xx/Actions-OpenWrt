5cfdaccd5b822c2c741a0636f3331dbddabdfb1a
xx51xx <live2@qq.com>
2025-08-14 09:25:15 +0800

    -

--- config/zbt_we826-192.168.2.1/diy-part3.sh
+++ config/zbt_we826-192.168.2.1/diy-part3.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-
+# 修改设备树
+mv -f 'mt7620a_phicomm_psg1208.dts' 'target/linux/ramips/dts'
+# 修改 内存大小
+sed -i 's/IMAGE_SIZE := 7872k/IMAGE_SIZE := 16064k/g' lede/target/linux/ramips/image/mt7620.mk
 exit 0
