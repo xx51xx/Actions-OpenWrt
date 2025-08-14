cefd656ba178c527fb8f1125721c594950fda08c
xx51xx <live2@qq.com>
2025-08-14 09:27:19 +0800

    -

--- config/zbt_we826-192.168.2.1/diy-part3.sh
+++ config/zbt_we826-192.168.2.1/diy-part3.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # 修改设备树
-mv -f 'mt7620a_phicomm_psg1208.dts' 'target/linux/ramips/dts'
+mv -f '$WORKSPACE_DIR/config/$cfg/mt7620a_phicomm_psg1208.dts' target/linux/ramips/dts
+cat target/linux/ramips/dts/mt7620a_phicomm_psg1208.dts
 # 修改 内存大小
-sed -i 's/IMAGE_SIZE := 7872k/IMAGE_SIZE := 16064k/g' lede/target/linux/ramips/image/mt7620.mk
+sed -i 's/IMAGE_SIZE := 7872k/IMAGE_SIZE := 16064k/g' target/linux/ramips/image/mt7620.mk
+cat target/linux/ramips/image/mt7620.mk
 exit 0
